ae9c2d47816223457da5f4865bcdc0f0f5a8f571
ManhCuongN <nmcuong.cpr@gmail.com>
2023-12-20 00:03:42 +0700

    deloy 1

---
+++ server/routes/dalle.routes.js
@@ -0,0 +1,62 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { Configuration, OpenAIApi} from 'openai';
+
+dotenv.config();
+
+const router = express.Router();
+
+const config = new Configuration({
+  apiKey: "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"
+});
+
+const openai = new OpenAIApi(config);
+
+router.route('/').get((req, res) => {
+  res.status(200).json({ message: "Hello from DALL.E ROUTES" })
+})
+
+router.route('/').post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b64_json'
+    });
+
+    const image = response.data.data[0].b64_json;
+
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" })
+  }
+})
+
+router.route('/chat').post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+console.log(prompt);
+    const resP = await openai.listEngines()
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+      max_tokens: 1000,
+      temperature: 0,
+    });
+    console.log(response.data);
+    if(response.data.choices[0].text) {
+      res.json({
+      message: response.data.choices[0].text
+    })
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" })
+  }
+})
+
+export default router;
